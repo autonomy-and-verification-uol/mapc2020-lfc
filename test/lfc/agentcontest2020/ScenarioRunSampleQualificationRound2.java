fc99e780413cce7759a5f990d26a72b4f315e760
Rafael C. Cardoso <rafael_caue@hotmail.com>
2020-08-04 17:59:54 +0100

    Updating config files and adding qualification scenarios.

---
+++ test/lfc/agentcontest2020/ScenarioRunSampleQualificationRound2.java
@@ -0,0 +1,83 @@
+package lfc.agentcontest2020;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Before;
+import org.junit.Test;
+
+import jacamo.infra.JaCaMoLauncher;
+import jason.JasonException;
+import massim.Server;
+
+
+public class ScenarioRunSampleQualificationRound2 {
+	
+	@Before
+	public void cleanUpFolders() throws IOException {
+
+		File currentDir = new File("");
+		String path = currentDir.getAbsolutePath();	
+				
+		ScenarioRunSampleQualificationRound2 deletefiles = new ScenarioRunSampleQualificationRound2();
+		deletefiles.delete(5, path + "/logs");
+		deletefiles.delete(4, path + "/log");
+		deletefiles.delete(5, path + "/replays");	
+		
+	}
+	
+	public void delete(long nFiles, String directoryFolder) throws IOException {
+		File folder = new File(directoryFolder);
+		if(folder.exists()) {
+			File[] listFiles = folder.listFiles();
+			Arrays.sort(listFiles);
+			for ( int i=0; i < listFiles.length - nFiles ; i++ ){
+				if (!listFiles[i].getName().equals(".keepfolder")) {
+//					System.out.println(listFiles[i].getName());
+					listFiles[i].delete();
+					FileUtils.deleteDirectory(listFiles[i]);
+				}
+			}		
+		}
+	}		
+	
+	@Before
+	public void setUp() {
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					
+					Server.main(new String[] {"-conf", "conf/SampleQualification2.json", "--monitor"});				
+					
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+
+		try {			
+			JaCaMoLauncher.main(new String[] {"lfc-mapc2020-50.jcm"});
+		} catch (JasonException e) {
+			System.out.println("Exception: "+e.getMessage());
+			e.printStackTrace();
+		}
+
+	}
+	
+	@Test
+	public void run() {		
+	}
+
+		
+}
+
+
+
+
+
+
+
